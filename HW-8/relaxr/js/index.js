7282a213efc048ffd8086aacfac0aa55461c1cd5
Thierry <thierry.heumann@gmail.com>
2016-02-15 14:21:28 -0800

    HW8 bis

--- HW-8/relaxr/js/index.js
+++ HW-8/relaxr/js/index.js
@@ -2,19 +2,7 @@ $(document).ready(function () {
 
   $('form').submit(function(evt) {
     evt.preventDefault();
-/*    var showAlert = true;
-    $('form input').each(function(){
-    if (!$(this).val()) {
-      $(this).removeClass("valid").addClass("invalid");
-      if (showAlert = true) {
-        alert("Please fill all required fields.");
-        showAlert = false;
-      }
-    }
-    else {
-      !$(this).removeClass("invalid").addClass("valid");
-    }
-  });*/
+    
     $('form input').each (function(){
       var flag = true;
       if ($(this).val() === '') {
